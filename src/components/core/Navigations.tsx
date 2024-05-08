fd74c641e17add0b843fd706c1c0a47fc762c7cb
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-05-08 20:33:44 +0700

    nav-core fix

--- src/components/core/Navigations.tsx
+++ src/components/core/Navigations.tsx
@@ -3,56 +3,58 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/navbar-menu";
 import { cn } from "@/utils/cn";
 
-
 interface NavigationProps {
-    title: string;
-    link: string;
+  title: string;
+  link: string;
 }
 
 //create an array of tile and subtitle
 const navigationData: NavigationProps[] = [
-    {
-        title: "Home",
-        link: "/index",
-    },
-    {
-        title: "About",
-        link: "/About",
-    },
-    {
-        title: "Projects",
-        link: "/Projects",
-    },
-    {
-        title: "Contact",
-        link: "/Contact",
-    },
+  {
+    title: "Home",
+    link: "/index",
+  },
+  {
+    title: "About",
+    link: "/About",
+  },
+  {
+    title: "Projects",
+    link: "/Projects",
+  },
+  {
+    title: "Contact",
+    link: "/Contact",
+  },
 ];
 
-export default function Navigation( {className}: {className?: string}) {
-    const [active, setActive] = useState<string | null>(null);
+export default function Navigation({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
 
-    return (
-        <div
-        className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-      >
-        <Menu setActive={setActive}>
-          {navigationData.map((item, index) => (
-            <MenuItem key={index} setActive={function (item: string): void {
-              throw new Error("Function not implemented.");
-            } } active={null} item={""}>
-              <Link href={item.link}>
-                <HoveredLink
-                  onMouseEnter={() => setActive(item.title)}
-                  onMouseLeave={() => setActive(null)}
-                  active={active === item.title}
-                >
-                  {item.title}
-                </HoveredLink>
-              </Link>
-            </MenuItem>
-          ))}
-        </Menu>
-      </div>
-    );
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+      {navigationData.map((item, index) => (
+        <MenuItem
+          key={index}
+          setActive={setActive}
+          active={active === item.title ? 'true' : 'false'}
+          item={item.title}
+        >
+          <Link href={item.link}>
+            <HoveredLink
+              onMouseEnter={() => setActive(item.title)}
+              onMouseLeave={() => setActive(null)}
+              active={active === item.title ? "true" : "false"} 
+            >
+              {item.title}
+            </HoveredLink>
+          </Link>
+        </MenuItem>
+      ))}
+      </Menu>
+    </div>
+  );
 }
