d14507c657e692797224be254ab4898398f4acc5
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-12-02 21:07:43 +0700

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { Container, Title, Text, Flex } from "@mantine/core"
+
 export default function Home() {
   return (
-    <header>HOME</header>
+    <Container>
+      <Flex direction="row" justify="center">
+        <Flex direction="column" justify="center">
+          <Title order={1}>Hello !</Title>
+          <Title order={2}>Anascence</Title>
+        </Flex>
+
+        <Flex direction="column" justify="center">
+        </Flex>
+      </Flex>
+    </Container>
   );
 }
