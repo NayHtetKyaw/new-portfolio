b15e46d2ab869ebae2d450aa2611b2644e694e62
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-12-06 15:18:47 +0700

    build error fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,15 @@
 import { Box, Stack, Title, Text, Container, Card, Flex } from "@mantine/core";
-import { IconCode, IconLabel, IconFileDescription, IconBrandGit, IconBrandTypescript } from "@tabler/icons-react";
-import React, { ReactElement } from "react";
+import { IconCode, IconLabel, IconFileDescription, IconBrandGit } from "@tabler/icons-react";
 import Link from "next/link";
 
-interface Projects {
+interface Project {
   name: string;
   description: string;
   technologies: string;
-  repo: ReactElement[];
+  repos?: {
+    link: string;
+    text: string;
+  }[];
 }
 
 const fieldIcons = {
@@ -17,54 +19,75 @@ const fieldIcons = {
   repo: <IconBrandGit size={25} className="flex-shrink-0" />
 }
 
-const projects: Projects[] = [
+const projects: Project[] = [
   {
     name: "Course Compose",
     description: "Web application for reviewing courses at Stamford International University",
     technologies: "NextJs, TypesScript, GoLang, Tailwind, Mantine/core, Redis, Supabase, Posgresql, Docker",
-    repo: [
-      <Link href="https://github.com/stamford-syntax-club/course-compose" target="_blank" className="text-sky-700">/stamford-syntax-club/course-compose</Link>
+    repos: [
+      {
+        link: "https://github.com/stamford-syntax-club/course-compose",
+        text: "/stamford-syntax-club/course-compose",
+      },
     ]
   },
   {
     name: "Stamford Center",
     description: "A centralized web application for accessing all kind of documents and resources needed as a student at Stamford",
     technologies: "NextJs, TypesScript, Javascript, Tailwind, Mantine/core, Redis, Docker",
-    repo: [
-      <Link href="https://github.com/stamford-syntax-club/stamfordcenter-frontend.git" target="_blank" className="text-sky-700">/stamford-syntax-club/stamfordcenter-frontend</Link>,
-      <Link href="https://github.com/stamford-syntax-club/stamfordcenter-backend.git" target="_blank" className="text-sky-700 ml-5">/stamford-syntax-club/stamfordcenter-backend</Link>
+    repos: [
+      {
+        link: "https://github.com/stamford-syntax-club/stamfordcenter-frontend.git",
+        text: "/stamford-syntax-club/stamfordcenter-frontend",
+      },
+      {
+        link: "https://github.com/stamford-syntax-club/stamfordcenter-backend.git",
+        text: "/stamford-syntax-club/stamfordcenter-backend",
+      }
     ]
   },
   {
     name: "Style War",
     description: "A customized coding competition platform that allow users to write codes and preview the result at the same time. And allow judges and admin to view all the competitor's result overview and code detials",
     technologies: "NextJs, TypesScript, GoLang, Tailwind, Mantine/core, WebSocket, Clerk auth, Docker",
-    repo: [
-      <Link href="https://github.com/stamford-syntax-club/style-war" target="_blank" className="text-sky-700">/stamford-syntax-club/style-war</Link>
+    repos: [
+      {
+        link: "https://github.com/stamford-syntax-club/style-war",
+        text: "/stamford-syntax-club/style-war",
+      }
     ]
   },
   {
     name: "My Spotify Taste",
     description: "A web platform to view all the detials about your spotify listening stats",
     technologies: "NextJs, TypesScript, GoLang, Tailwind, Mantine/core, SpotifyWebApi",
-    repo: [
-      <Link href="https://github.com/stamford-syntax-club/style-war" target="_blank" className="text-sky-700">/NayHtetKyaw/mySpotifyTaste</Link>
+    repos: [
+      {
+        link: "https://github.com/NayHtetKyaw/mySpotifyTaste",
+        text: "/NayHtetKyaw/mySpotifyTaste",
+      }
     ]
   },
   {
     name: "CoffeeShop",
     description: "A web platform functions as the online shop to order food & beverages",
     technologies: "HTML, CSS, PHP, MYSQL, JavaScript",
-    repo: [
-      <Link href="https://github.com/NayHtetKyaw/coffeeshop.git" target="_blank" className="text-sky-700">/NayHtetKyaw/coffeeshop</Link>
+    repos: [
+      {
+        link: "https://github.com/NayHtetKyaw/coffeeshop.git",
+        text: "/NayHtetKyaw/coffeeshop",
+      }
     ]
   },
   {
     name: "Redis Client App",
     description: "Platform to test out redis database",
     technologies: "Typescript, nextjs, redis, Javascript",
-    repo: [
-      <Link href="https://github.com/NayHtetKyaw/redis-client-app.git" target="_blank" className="text-sky-700">/NayHtetKyaw/redis-client-app</Link>
+    repos: [
+      {
+        link: "https://github.com/NayHtetKyaw/redis-client-app.git",
+        text: "/NayHtetKyaw/redis-client-app",
+      }
     ]
   },
 ]
@@ -80,7 +103,6 @@ export default function Projects() {
               <Flex mb="sm">
                 <Title order={2}>{item.name}</Title>
               </Flex>
-
               <Flex align="center" gap="sm" mb="sm">
                 {fieldIcons.description}
                 <Text>{item.description}</Text>
@@ -93,7 +115,11 @@ export default function Projects() {
 
               <Flex align="center" gap="sm" mb="sm">
                 {fieldIcons.repo}
-                <Text>{item.repo}</Text>
+                {item.repos?.map((repo, repoIndex) => (
+                  <Link key={`${index}-repo-${repoIndex}`} href={repo.link} target="_blank">
+                    <Text>{repo.text}</Text>
+                  </Link>
+                ))}
               </Flex>
             </Card>
           ))}
